5a0a50da6a374b5d118e921322d5f25bfe9dc823
karlshaver <karl@meshfresh.com>
2017-09-28 17:42:06 -0400

    Staging commit, styling and markup changes

--- footer.php
+++ footer.php
@@ -10,16 +10,17 @@
 				</nav> -->
 				<h6>Contact Us</h6>
 				<p class="address">
-					123 Example Street<br>Brooklyn, NY 12345
+					Caton Flats<br>123 Example Street<br>Brooklyn, NY 12345
 				</p>
-				<a class="email" href="mailto:catonflats@example.com">catonflats@example.com</a>
-			</div>
-			<div class="columns-4">
-				<p class="copyright">&#169; Caton Flats Development</p>
-				<p>Site by <a href="http://meshfresh.com" target="_blank">MESH</a></p>
+				<p><a class="email" href="mailto:catonflats@example.com">catonflats@example.com</a></p>
 			</div>
 			<div class="columns-4 centered-section">
 				<a class="scroll-link button" href="#welcome">Back to Top</a>
+			</div>
+			<div class="columns-4">
+				<h6>Info</h6>
+				<p class="copyright">&#169; Caton Flats Development</p>
+				<p>Site by <a href="http://meshfresh.com" target="_blank">MESH</a></p>
 			</div>
 		</div>
 	</div>
